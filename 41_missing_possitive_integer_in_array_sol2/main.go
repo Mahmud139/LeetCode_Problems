a142ce386f47e634b71d0ee56f29e12c8e7c0a42
mahmud139 <mahmudulhasan139139@gmail.com>
2022-11-30 19:54:00 +0600

    updated

--- 41_missing_possitive_integer_in_array_sol2/main.go
+++ 41_missing_possitive_integer_in_array_sol2/main.go
@@ -1,11 +1,44 @@
 package main
 
-import "fmt"
-
-func main() {
-
-}
 
 func firstMissingPositive(nums []int) int {
-	lenght := len(nums)
+	// Linear time, and constant extra space.
+	// Unrealistic solution, because it loses all the values initially <= 0:
+	// the negative numbers already in the array can't be recovered without extra space for remembering them.
+	// It's fine if we don't care about the values, or all the values are positive.
+
+	limit := len(nums)
+	// For correctness, ensure no value is negative,
+	// making all of them between 1 and limit + 1, both included.
+	for i, num := range nums {
+		if num <= 0 || num > limit {
+			nums[i] = limit + 1
+		}
+	}
+
+	// Mark the appearance of positive number x (1, 2, ..., limit)
+	// by negating the value at position x-1 (0, 1, ..., limit-1)
+	for _, num := range nums {
+		absNum := num
+		if absNum < 0 {
+			absNum = -absNum
+		}
+		if absNum > limit {
+			continue
+		}
+		appearanceMarker := nums[absNum-1]
+		if appearanceMarker > 0 {
+			nums[absNum-1] = -appearanceMarker
+		}
+	}
+
+	// Look for the first unmarked position, corresponding to number position+1
+	position := 0
+	for position < limit {
+		if nums[position] > 0 {
+			break
+		}
+		position++
+	}
+	return position + 1
 }
